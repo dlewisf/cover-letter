fd89922e5750121c5aa005c01f92b7888bf493af
dlewisf <dlewisf@gmail.com>
2019-05-11 00:43:38 -0500

    Add svg-injector

--- server/main.js
+++ server/main.js
@@ -4,7 +4,9 @@ const app = express();
 const package = require('../package.json');
 
 app.use(express.static('public'));;
+app.use('/vendor', express.static(path.join(__dirname , '..', 'node_modules')));
 app.get('/', (req, res)=> res.send(path.join(__dirname , '..', 'public/index.html')));
+
 
 app.listen(package.config.PORT, () => {
   console.log(`Listening on port ${package.config.PORT}`);
